d1db0a4a3788928991b8cb98e9aa26d2c6a5f647
José Fran Morales Ruiz <61171846+josmorrui2@users.noreply.github.com>
2021-05-16 17:36:26 +0200

    Horario dinámico

--- CalendarioV2/app/src/main/java/com/example/calendariov2/HorarioActivity.java
+++ CalendarioV2/app/src/main/java/com/example/calendariov2/HorarioActivity.java
@@ -3,12 +3,42 @@ package com.example.calendariov2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class HorarioActivity extends AppCompatActivity {
+
+    TableLayout tableLayout;
+    TableRow tableRow;
+    EditText txtCell;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horario);
+        tableLayout = findViewById(R.id.tbHorario);
+
+        AddRow(tableLayout);
+    }
+
+    public void AddRow(View view) {
+
+        TableRow.LayoutParams tableParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT);
+        tableRow = new TableRow(this);
+        tableRow.setLayoutParams(tableParams);
+        tableParams = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
+
+        for (int j = 0; j < 5; j++)
+        {
+            txtCell = new EditText(this);
+            txtCell.setGravity(Gravity.CENTER);
+            txtCell.setLayoutParams(tableParams);
+            txtCell.setHint("" + j);
+            tableRow.addView(txtCell);
+        }
+        tableLayout.addView(tableRow);
     }
 }
--- CalendarioV2/app/src/main/java/com/example/calendariov2/MainActivity.java
+++ CalendarioV2/app/src/main/java/com/example/calendariov2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -22,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
                 /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
                 Intent form = new Intent(getApplicationContext(), FormExamenesActivity.class);
+                startActivityForResult(form, 1);
+            }
+        });
+
+        Button btnHorario = findViewById(R.id.btnHorario);
+        btnHorario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent form = new Intent(getApplicationContext(), HorarioActivity.class);
                 startActivityForResult(form, 1);
             }
         });
